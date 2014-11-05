829230f1fc8734622c566cbfba3534b6b3b7d55d
Lasse Borchard <lasse.borchard@prosiebensat1digital.de>
2014-11-05 15:56:32 +0100

    fix syntax error

--- manifests/user.pp
+++ manifests/user.pp
@@ -6,11 +6,11 @@ class tomcat::user {
       gid    => $::tomcat::tomcat_gid,
       system => true,
       home   => $::tomcat::instance_basedir,
-    } else {
-      exec{'modify tomcat user home':
-        command => "/usr/sbin/usermod -m -d ${::tomcat::instance_basedir} ${::tomcat::tomcat_user}",
-        unless  => "/bin/egrep '(${::tomcat::tomcat_user}.*:${::tomcat::instance_basedir}:)' /etc/passwd",
-      }
     }
-  }
+  } else {
+    exec{'modify tomcat user home':
+      command => "/usr/sbin/usermod -m -d ${::tomcat::instance_basedir} ${::tomcat::tomcat_user}",
+      unless  => "/bin/egrep '(${::tomcat::tomcat_user}.*:${::tomcat::instance_basedir}:)' /etc/passwd",
+    }
+ }
 }
