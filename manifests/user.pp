176d4229e6f6511de9579938eab2909727e79a3d
Lasse Borchard <lasse.borchard@prosiebensat1digital.de>
2014-11-05 17:11:25 +0100

    create only the user if its the default user

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,5 +1,7 @@
+# create only the user if its the default user
+# else just homedirctory will be changed
 class tomcat::user {
-  if ! defined(User[$::tomcat::tomcat_user]) {
+  if $::tomcat::tomcat_user == 'tomcat' {
     user { $::tomcat::tomcat_user :
       ensure => present,
       uid    => $::tomcat::tomcat_uid,
@@ -11,6 +13,7 @@ class tomcat::user {
     exec{'modify tomcat user home':
       command => "/usr/sbin/usermod -m -d ${::tomcat::instance_basedir} ${::tomcat::tomcat_user}",
       unless  => "/bin/egrep '(${::tomcat::tomcat_user}.*:${::tomcat::instance_basedir}:)' /etc/passwd",
+      require => User[$::tomcat::tomcat_user],
     }
   }
 }
